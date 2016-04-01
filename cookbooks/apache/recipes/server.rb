a37c15fb74401e882a40c4c3a68a2e432e22b4bb
johnfitzpatrick <jhnftzptrck@gmail.com>
2016-04-01 14:13:30 +0100

    Switched back to `file` resource - cover templates later.

--- cookbooks/apache/recipes/server.rb
+++ cookbooks/apache/recipes/server.rb
@@ -5,8 +5,11 @@
 # Copyright (c) 2016 The Authors, All Rights Reserved.
 package 'httpd'
 
-template '/var/www/html/index.html' do
-  source 'index.html.erb'
+file '/var/www/html/index.html' do
+  content "<h1>Hello, world!</h1>
+<h2>ipaddress: #{node['ipaddress']}</h2>
+<h2>hostname: #{node['hostname']}</h2>
+"
 end
 
 service 'httpd' do
